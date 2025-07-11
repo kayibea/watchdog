89ba999b920bfa93354cf205a61bf40780764746
kayibea <gatse1531@gmail.com>
2025-07-11 04:29:46 +0200

    🥅 Add error handling, the traditional nil, error, errno triplet on error like io.open

--- watchdog.c
+++ watchdog.c
@@ -24,39 +24,11 @@ typedef struct {
   int cb_table_ref;
 } Watchdog;
 
-static int l_add(lua_State *L) {
-  Watchdog *wd = luaL_checkudata(L, 1, WATCHDOG_MT);
-  const char *path = luaL_checkstring(L, 2);
-  uint32_t mask = luaL_checkinteger(L, 3);
-  luaL_checktype(L, 4, LUA_TFUNCTION);
-
-  int watch_fd = inotify_add_watch(wd->fd, path, mask);
-  if (watch_fd < 0)
-    return luaL_error(L, "inotify_add_watch failed: %s", strerror(errno));
-
-  lua_rawgeti(L, LUA_REGISTRYINDEX, wd->cb_table_ref);
-  lua_pushvalue(L, 4);
-  lua_rawseti(L, -2, watch_fd);
-  lua_pop(L, 1);
-
-  lua_pushinteger(L, watch_fd);
-  return 1;
-}
-
-static int l_remove(lua_State *L) {
-  Watchdog *wd = luaL_checkudata(L, 1, WATCHDOG_MT);
-  int watch_fd = luaL_checkinteger(L, 2);
-
-  if (inotify_rm_watch(wd->fd, watch_fd) < 0) {
-    return luaL_error(L, "inotify_rm_watch failed: %s", strerror(errno));
-  }
-
-  lua_rawgeti(L, LUA_REGISTRYINDEX, wd->cb_table_ref);
+static int handle_error(lua_State *L) {
   lua_pushnil(L);
-  lua_rawseti(L, -2, watch_fd);
-  lua_pop(L, 1);
-
-  return 0;
+  lua_pushstring(L, strerror(errno));
+  lua_pushinteger(L, errno);
+  return 3;
 }
 
 static void push_event(lua_State *L, struct inotify_event *ev) {
@@ -79,6 +51,39 @@ static void push_event(lua_State *L, struct inotify_event *ev) {
   lua_settable(L, -3);
 }
 
+static int l_add(lua_State *L) {
+  Watchdog *wd = luaL_checkudata(L, 1, WATCHDOG_MT);
+  const char *path = luaL_checkstring(L, 2);
+  uint32_t mask = luaL_checkinteger(L, 3);
+  luaL_checktype(L, 4, LUA_TFUNCTION);
+
+  int watch_fd = inotify_add_watch(wd->fd, path, mask);
+  if (watch_fd < 0) return handle_error(L);
+
+  lua_rawgeti(L, LUA_REGISTRYINDEX, wd->cb_table_ref);
+  lua_pushvalue(L, 4);
+  lua_rawseti(L, -2, watch_fd);
+  lua_pop(L, 1);
+
+  lua_pushinteger(L, watch_fd);
+  return 1;
+}
+
+static int l_remove(lua_State *L) {
+  Watchdog *wd = luaL_checkudata(L, 1, WATCHDOG_MT);
+  int watch_fd = luaL_checkinteger(L, 2);
+
+  if (inotify_rm_watch(wd->fd, watch_fd) < 0) return handle_error(L);
+
+  lua_rawgeti(L, LUA_REGISTRYINDEX, wd->cb_table_ref);
+  lua_pushnil(L);
+  lua_rawseti(L, -2, watch_fd);
+  lua_pop(L, 1);
+
+  lua_pushinteger(L, 0);
+  return 1;
+}
+
 static int l_poll(lua_State *L) {
   Watchdog *wd = luaL_checkudata(L, 1, WATCHDOG_MT);
   int timeout = luaL_optinteger(L, 2, -1);
@@ -89,11 +94,19 @@ static int l_poll(lua_State *L) {
   };
 
   int ret = poll(&pfd, 1, timeout);
-  if (ret <= 0) return 0;
+  if (ret < 0) return handle_error(L);
+  if (ret == 0) {
+    lua_pushinteger(L, ret);
+    return 1;
+  }
 
   char buf[EVENT_BUF_LEN];
   ssize_t len = read(wd->fd, buf, sizeof(buf));
-  if (len <= 0) return 0;
+  if (len < 0) return handle_error(L);
+  if (len == 0) {
+    lua_pushinteger(L, len);
+    return 1;
+  };
 
   lua_rawgeti(L, LUA_REGISTRYINDEX, wd->cb_table_ref);
 
@@ -116,24 +129,27 @@ static int l_poll(lua_State *L) {
   }
 
   lua_pop(L, 1);
-  return 0;
+  lua_pushinteger(L, ret);
+  return 1;
 }
 
 static int l_close(lua_State *L) {
   Watchdog *wd = luaL_checkudata(L, 1, WATCHDOG_MT);
+  int ret = 0;
   if (wd->fd >= 0) {
-    close(wd->fd);
     luaL_unref(L, LUA_REGISTRYINDEX, wd->cb_table_ref);
+    if ((ret = close(wd->fd)) < 0) return handle_error(L);
     wd->fd = -1;
   }
-  return 0;
+  lua_pushinteger(L, ret);
+  return 1;
 }
 
 static int l_gc(lua_State *L) { return l_close(L); }
 
 static int l_init(lua_State *L) {
   int fd = inotify_init1(IN_NONBLOCK);
-  if (fd < 0) return luaL_error(L, "inotify_init1 failed: %s", strerror(errno));
+  if (fd < 0) return handle_error(L);
 
   Watchdog *wd = lua_newuserdata(L, sizeof(Watchdog));
   wd->fd = fd;
@@ -196,7 +212,6 @@ int luaopen_watchdog(lua_State *L) {
   ADD_CONST(L, IN_MASK_ADD);
   ADD_CONST(L, IN_ISDIR);
   ADD_CONST(L, IN_ONESHOT);
-
   ADD_CONST(L, IN_CLOSE);
   ADD_CONST(L, IN_MOVE);
   ADD_CONST(L, IN_ALL_EVENTS);
